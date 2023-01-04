57a189ee452e7d23b5dd1554139f3de5bb9f0c8c
Branden Kennedy <brandenkenn@gmail.com>
2023-01-04 13:47:12 -0500

    formatting previous 7 kyu

--- 7 Kyu/stringEndsWith.js
+++ 7 Kyu/stringEndsWith.js
@@ -1,7 +1,7 @@
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 function solution(string, ending){
-    if (ending.length === 0) {
+    if (ending.length == 0) {
         return true
     } else {
         return string.slice(-ending.length) == ending
